pchickey/purple-ardupilot-mega

AP_InertialSensor: ported to AP_HAL

libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.h
@@ -3,11 +3,9 @@
 #ifndef __AP_INERTIAL_SENSOR_MPU6000_H__
 #define __AP_INERTIAL_SENSOR_MPU6000_H__
 
-#include <string.h>
 #include <stdint.h>
 
-#include "../AP_PeriodicProcess/AP_PeriodicProcess.h"
-#include "../AP_Math/AP_Math.h"
+#include <AP_Math.h>
 #include "AP_InertialSensor.h"
 
 #define MPU6000_CS_PIN       53        // APM pin connected to mpu6000's chip select pin
@@ -22,7 +20,7 @@ public:
 
     AP_InertialSensor_MPU6000();
 
-    uint16_t            init( AP_PeriodicProcess * scheduler );
+    uint16_t            init();
     static void         dmp_init(); // Initialise MPU6000's DMP
     static void         dmp_reset();    // Reset the DMP (required for changes in gains or offsets to take effect)
 
@@ -80,8 +78,6 @@ private:
     static const int8_t         _accel_data_sign[3];
 
     static const uint8_t        _temp_data_index;
-
-    static AP_PeriodicProcess*  _scheduler;             // pointer to scheduler so that we can suspend/resume scheduler when we pull data from the MPU6000
 
     // ensure we can't initialise twice
     bool                        _initialised;

libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.cpp
@@ -62,9 +62,9 @@ AP_InertialSensor_Oilpan::AP_InertialSensor_Oilpan( AP_ADC * adc ) :
     _accel.z = 0;
 }
 
-uint16_t AP_InertialSensor_Oilpan::init( AP_PeriodicProcess * scheduler)
+uint16_t AP_InertialSensor_Oilpan::init()
 {
-    _adc->Init(scheduler);
+    _adc->Init();
 
     _accel_mid.x    = (_x_high + _x_low) / 2;
     _accel_mid.y    = (_y_high + _y_low) / 2;

libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.h
@@ -3,11 +3,10 @@
 #ifndef __AP_INERTIAL_SENSOR_OILPAN_H__
 #define __AP_INERTIAL_SENSOR_OILPAN_H__
 
-#include <string.h>
 #include <stdint.h>
 
-#include "../AP_ADC/AP_ADC.h"
-#include "../AP_Math/AP_Math.h"
+#include <AP_ADC.h>
+#include <AP_Math.h>
 #include "AP_InertialSensor.h"
 
 class AP_InertialSensor_Oilpan : public AP_InertialSensor
@@ -17,7 +16,7 @@ public:
     AP_InertialSensor_Oilpan( AP_ADC * adc );
 
     /* Concrete implementation of AP_InertialSensor functions: */
-    uint16_t        init(AP_PeriodicProcess * scheduler);
+    uint16_t        init();
     bool            update();
     bool            new_data_available();
     float           gx();

libraries/AP_InertialSensor/AP_InertialSensor_Stub.cpp
@@ -2,7 +2,7 @@
 
 #include "AP_InertialSensor_Stub.h"
 
-uint16_t AP_InertialSensor_Stub::init( AP_PeriodicProcess * scheduler ) {
+uint16_t AP_InertialSensor_Stub::init() {
     return AP_PRODUCT_ID_NONE;
 }
 

unluckyprince/ardupilot-mega

libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.h
@@ -3,11 +3,9 @@
 #ifndef __AP_INERTIAL_SENSOR_MPU6000_H__
 #define __AP_INERTIAL_SENSOR_MPU6000_H__
 
-#include <string.h>
 #include <stdint.h>
 
-#include "../AP_PeriodicProcess/AP_PeriodicProcess.h"
-#include "../AP_Math/AP_Math.h"
+#include <AP_Math.h>
 #include "AP_InertialSensor.h"
 
 #define MPU6000_CS_PIN       53        // APM pin connected to mpu6000's chip select pin
@@ -52,7 +50,7 @@ public:
     uint32_t            get_delta_time_micros();
 
 protected:
-    uint16_t                    _init_sensor( AP_PeriodicProcess * scheduler );
+    uint16_t                    _init_sensor();
 
 private:
 
@@ -75,8 +73,6 @@ private:
     static const int8_t         _accel_data_sign[3];
 
     static const uint8_t        _temp_data_index;
-
-    static AP_PeriodicProcess*  _scheduler;             // pointer to scheduler so that we can suspend/resume scheduler when we pull data from the MPU6000
 
     // ensure we can't initialise twice
     bool                        _initialised;

libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.cpp
@@ -41,9 +41,9 @@ AP_InertialSensor_Oilpan::AP_InertialSensor_Oilpan( AP_ADC * adc ) :
 {
 }
 
-uint16_t AP_InertialSensor_Oilpan::_init_sensor( AP_PeriodicProcess * scheduler)
+uint16_t AP_InertialSensor_Oilpan::_init_sensor()
 {
-    _adc->Init(scheduler);
+    _adc->Init();
 
 #if defined(DESKTOP_BUILD)
     return AP_PRODUCT_ID_SITL;

libraries/AP_InertialSensor/AP_InertialSensor_Oilpan.h
@@ -3,11 +3,10 @@
 #ifndef __AP_INERTIAL_SENSOR_OILPAN_H__
 #define __AP_INERTIAL_SENSOR_OILPAN_H__
 
-#include <string.h>
 #include <stdint.h>
 
-#include "../AP_ADC/AP_ADC.h"
-#include "../AP_Math/AP_Math.h"
+#include <AP_ADC.h>
+#include <AP_Math.h>
 #include "AP_InertialSensor.h"
 
 class AP_InertialSensor_Oilpan : public AP_InertialSensor
@@ -34,7 +33,7 @@ public:
     uint16_t        num_samples_available();
 
 protected:
-    uint16_t        _init_sensor(AP_PeriodicProcess * scheduler);
+    uint16_t        _init_sensor();
 
 private:
 

libraries/AP_InertialSensor/AP_InertialSensor_Stub.cpp
@@ -2,7 +2,7 @@
 
 #include "AP_InertialSensor_Stub.h"
 
-uint16_t AP_InertialSensor_Stub::_init_sensor( AP_PeriodicProcess * scheduler ) {
+uint16_t AP_InertialSensor_Stub::_init_sensor() {
     return AP_PRODUCT_ID_NONE;
 }
 