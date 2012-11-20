0298d0e6812c5509a5fcb1062c5d6699db446b97
Pat Hickey <pat@galois.com>
2012-11-19 16:41:28 -0800

    Filter: third order comp filter ported to AP_HAL. (Builds, not tested.)

--- libraries/Filter/ThirdOrderCompFilter.cpp
+++ libraries/Filter/ThirdOrderCompFilter.cpp
@@ -1,12 +1,5 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-#include <FastSerial.h>
-#include <ThirdOrderCompFilter.h>
-
-#if defined(ARDUINO) && ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include <wiring.h>
-#endif
+#include "ThirdOrderCompFilter.h"
 
 // Public Methods //////////////////////////////////////////////////////////////
 
